11bcea9e362a1a4a00f360be090fdd4cf634923f
Rodrigo <rodrigo_l321@hotmail.com>
2021-11-09 11:06:32 -0500

    Agregado de Eureka server

---
+++ spring-boot-eureka-server/src/main/java/com/formacionbdi/springboot/app/eureka/SpringBootEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEurekaServerApplication.class, args);
+	}
+
+}
--- spring-boot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ spring-boot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -15,7 +15,7 @@ import com.formacionbdi.springboot.app.item.models.service.ItemService;
 public class ItemController {
 	
 	@Autowired
-	@Qualifier("restTemplate")
+	@Qualifier("serviceFeign")
 	private ItemService itemService;
 	
 	@GetMapping("/listar")
