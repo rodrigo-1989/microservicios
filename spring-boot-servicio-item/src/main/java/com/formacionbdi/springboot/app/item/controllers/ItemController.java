3f08e08f81fdfffdcfba4bac962ddd54413ecfa6
Rodrigo <rodrigo_l321@hotmail.com>
2021-11-08 22:27:19 -0500

    Balanceo de cargas con RestTemplate

--- spring-boot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ spring-boot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -15,7 +15,7 @@ import com.formacionbdi.springboot.app.item.models.service.ItemService;
 public class ItemController {
 	
 	@Autowired
-	@Qualifier("serviceFeign")
+	@Qualifier("restTemplate")
 	private ItemService itemService;
 	
 	@GetMapping("/listar")
--- spring-boot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
+++ spring-boot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
@@ -13,14 +13,14 @@ import org.springframework.web.client.RestTemplate;
 import com.formacionbdi.springboot.app.item.models.Item;
 import com.formacionbdi.springboot.app.item.models.Producto;
 
-@Service
+@Service("restTemplate")
 public class ItemServiceImpl implements ItemService {
 
 	@Autowired
 	private RestTemplate clienteRest;
 	@Override
 	public List<Item> findAll() {
-		List<Producto> productos =Arrays.asList( clienteRest.getForObject("http://localhost:8001/listar",Producto.class ));
+		List<Producto> productos =Arrays.asList( clienteRest.getForObject("http://servicio-productos/listar",Producto.class ));
 		
 		return productos.stream().map( p ->new Item(p,1)).collect(Collectors.toList());
 	}
@@ -30,7 +30,7 @@ public class ItemServiceImpl implements ItemService {
 		
 		Map<String,String> pathVariables = new HashMap<String,String>();
 		pathVariables.put("id", id.toString());
-		Producto producto = clienteRest.getForObject("http://localhost:8001/listar/{id}",Producto.class,pathVariables );
+		Producto producto = clienteRest.getForObject("http://servicio-productos/listar/{id}",Producto.class,pathVariables );
 		
 		return new Item(producto,cantidad);
 	}
