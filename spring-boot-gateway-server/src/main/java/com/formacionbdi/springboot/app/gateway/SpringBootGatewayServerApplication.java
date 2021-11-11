7adbaf854fc02a0075b7f3f74f7e5d2d5ee9902e
Rodrigo <rodrigo_l321@hotmail.com>
2021-11-11 18:45:11 -0500

    Agregado de Gateway server

---
+++ spring-boot-gateway-server/src/main/java/com/formacionbdi/springboot/app/gateway/SpringBootGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGatewayServerApplication.class, args);
+	}
+
+}
