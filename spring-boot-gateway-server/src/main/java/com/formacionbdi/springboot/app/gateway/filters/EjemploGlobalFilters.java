9a6e5ac911b03142c680a253a9421e9599e526d7
Rodrigo <rodrigo_l321@hotmail.com>
2021-11-12 15:10:07 -0500

    Agregado de un token en las cabeceras de la respuesta

--- spring-boot-gateway-server/src/main/java/com/formacionbdi/springboot/app/gateway/filters/EjemploGlobalFilters.java
+++ spring-boot-gateway-server/src/main/java/com/formacionbdi/springboot/app/gateway/filters/EjemploGlobalFilters.java
@@ -1,9 +1,12 @@
 package com.formacionbdi.springboot.app.gateway.filters;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
@@ -12,18 +15,30 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class EjemploGlobalFilters implements GlobalFilter{
+public class EjemploGlobalFilters implements GlobalFilter, Ordered{
 	
 	private final Logger logger = LoggerFactory.getLogger(EjemploGlobalFilters.class);
 
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 		logger.info("Ejecuntado filtro PRE");
+		
+		exchange.getRequest().mutate().headers( h -> h.add("token","123456"));
+		
 		return chain.filter(exchange).then(Mono.fromRunnable(()->{
 			logger.info("Ejecuntado filtro POST");
+			Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token")).ifPresent( valor ->{
+				exchange.getResponse().getHeaders().add("token", valor);
+			});
+			
 			exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "rojo").build());
 			exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
 		}));
+	}
+
+	@Override
+	public int getOrder() {
+		return -1;
 	}
 
 }
